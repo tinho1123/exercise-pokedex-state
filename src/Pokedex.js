6e4ec497a28beba73240efc84b40ea6b1da92161
Wellington <tinhoefaela@gmail.com>
2021-11-03 21:02:31 -0300

    Exercicio 1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+  constructor() {
+    super()
+
+    this.handleClick = this.handleClick.bind(this)
+
+    this.state = {
+      index: 0,
+    }
+  }
+    
+  handleClick() {
+    this.setState((valorAnterior, _props) => ({
+      index: valorAnterior.index + 1,
+    }))
+
+    if(this.state.index > pokemons.length -2){
+      this.setState((valorAnterior, _props) => ({
+        index: valorAnterior.index = 0,
+      }))
+  }
+}
+
     render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
+
+      return (
+            <>
+            <button onClick={this.handleClick}>{`Trocar pokemon`}</button>
+              <div className="pokedex">   
+                  {this.props.pokemons.map((pokemon, index) =>
+                  <>
+                  {index === this.state.index && <Pokemon key={pokemon.id} pokemon={pokemon} />}
+                  </>
+                  )}
+              </div>
+            </>
         );
     }
 }
